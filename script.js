929dc4f54de93af1c8cd52b12b9cde56a1653618
Roshan Singh <rosh.s568@gmail.com>
2024-04-02 20:43:58 +0530

    Add Search Function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const searchBox = document.querySelector("searchbar");
-const searchButton = document.querySelector("searchbutton");
+const searchBox = document.querySelector("#searchbar");
+const searchButton = document.querySelector("#searchbutton");
 
 const fetchData = async () => {
   try {
@@ -11,12 +11,29 @@ const fetchData = async () => {
   } catch (err) {
     console.log(err);
   }
-  console.log(data);
-
   document.querySelector("#temp").innerHTML = Math.round(data.main.temp) + "°c";
   document.querySelector("#city").innerHTML = data.name;
   document.querySelector("#humidity").innerHTML = data.main.humidity + "%";
   document.querySelector("#wind").innerHTML = data.wind.speed + "km/h";
 };
 
-fetchData();
+const getCityWeather = async (city) => {
+  console.log(city);
+  try {
+    const responce = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f38611ba8e07ee5d8eb2719988814b5a&units=metric` +
+        "&appid=${apiKey}"
+    );
+    var data = await responce.json();
+  } catch (err) {
+    console.log(err);
+  }
+  document.querySelector("#temp").innerHTML = Math.round(data.main.temp) + "°c";
+  document.querySelector("#city").innerHTML = data.name;
+  document.querySelector("#humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector("#wind").innerHTML = data.wind.speed + "km/h";
+};
+
+searchButton.addEventListener("click", () => getCityWeather(searchBox.value))
+
+// fetchData();
